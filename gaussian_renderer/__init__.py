fix nvec normalization bug

gaussian_renderer/__init__.py
@@ -12,6 +12,8 @@ import os
 from typing import Dict, Optional, Union
 
 import torch
+import torch.nn.functional as F
+
 import math
 from diff_surfel_rasterization import GaussianRasterizationSettings, GaussianRasterizer
 from scene.gaussian_model import GaussianModel
@@ -573,7 +575,6 @@ def pbr_render_deffered(viewpoint_camera, pc: GaussianModel,
     means3D = pc.get_xyz
     means2D = screenspace_points
     opacity = pc.get_opacity
-    numG = means3D.shape[0]
 
      # If precomputed 3d covariance is provided, use it. If not, then it will be computed from
     # scaling / rotation by the rasterizer.
@@ -595,16 +596,6 @@ def pbr_render_deffered(viewpoint_camera, pc: GaussianModel,
     else:
         scales = pc.get_scaling
         rotations = pc.get_rotation
-
-    #prepare all input for pbr: material, wi, wo, normals, light
-    view_pos = viewpoint_camera.camera_center.repeat(numG, 1) # (numG, 3)
-    wo_W = safe_normalize(view_pos - means3D) # (numG, 3) wo指向表面外侧
-
-    normalsG_W = pc.get_normals # (numG, 3)
-    # assume dual visiable, need to verify this，看看正负号对不对
-    cos = dot(normalsG_W, wo_W) # (numG, 1)
-    mul = torch.where(cos > 0, 1., -1.) # (numG, 1)
-    normalsG_W = normalsG_W * mul # (numG, 3)
 
     diffuse=pc.get_albedo
     roughness=pc.get_roughness
@@ -640,6 +631,13 @@ def pbr_render_deffered(viewpoint_camera, pc: GaussianModel,
     render_normal = allmap[2:5]
     render_normal = (render_normal.permute(1,2,0) @ (viewpoint_camera.world_view_transform[:3,:3].T)).permute(2,0,1)
 
+    render_normal = torch.where(
+        torch.norm(render_normal, dim=0, keepdim=True) > 0,
+        F.normalize(render_normal, dim=0, p=2),
+        render_normal,
+    )
+
+
     # get median depth map
     render_depth_median = allmap[5:6]
     render_depth_median = torch.nan_to_num(render_depth_median, 0, 0)
@@ -664,6 +662,11 @@ def pbr_render_deffered(viewpoint_camera, pc: GaussianModel,
     # remember to multiply with accum_alpha since render_normal is unnormalized.
     surf_normal = surf_normal * (render_alpha).detach()
 
+    surf_normal = torch.where(
+        torch.norm(surf_normal, dim=0, keepdim=True) > 0,
+        F.normalize(surf_normal, dim=0, p=2),
+        surf_normal,
+    )
 
     pre_blend = {
         # "kd_map": diffuse,
@@ -686,17 +689,15 @@ def pbr_render_deffered(viewpoint_camera, pc: GaussianModel,
             rotations = rotations,
             cov3D_precomp = cov3D_precomp)[0]
         deffered_input[k] = image
-        
+
     rgb, extras = gshader_deferred_shading(light, 
                          render_normal.permute(1,2,0).contiguous(), 
-                         view_dirs.permute(1,2,0).contiguous(), 
+                         view_dirs, 
                          deffered_input["kd_map"].permute(1,2,0).contiguous(), 
                          deffered_input["kr_map"][0,:,:][None,:,:].permute(1,2,0).contiguous(), 
                          deffered_input["ks_map"].permute(1,2,0).contiguous(), 
                          brdf_lut)
 
-    # print(rgb.shape)
-    # print(deffered_input["cr_map"].shape)
     if color_residual:
         rgb =rgb.permute(2,0,1) + deffered_input["cr_map"] # color residuals
     rgb = rgb.clamp(min=0.0, max=1.0)
@@ -704,7 +705,14 @@ def pbr_render_deffered(viewpoint_camera, pc: GaussianModel,
     # mask = (render_alpha >= 0.5).all(0)[None,:,:] # (1, H, W)
     # mask = (render_normal != 0).all(0, keepdim=True)
     # rgb_image = torch.where(mask, rgb, bg_color[:,None,None])
-    rgb_image = rgb
+
+    rgb_image = torch.where(
+        torch.norm(render_normal, dim=0, keepdim=True) > 0,
+        rgb,
+        bg_color[:,None,None],
+    )
+
+    # rgb_image = rgb
     rets =  {"render": rgb_image,
             "viewspace_points": means2D,
             "visibility_filter" : radii > 0,

pbr/shade.py
@@ -322,15 +322,16 @@ def gshader_deferred_shading(
     diffuse_raw = kd.reshape(1, H, W, 3)
     roughness = kr.reshape(1, H, W, 1)
     spec_col = ks.reshape(1, H, W, 3)
-    diff_col = 1.0 - ks
+    diff_col = 1.0 - spec_col
 
-    results = {}
     reflvec = (
-        2.0 * (normals * view_dirs).sum(-1, keepdims=True).clamp(min=0.0) * normals - view_dirs
+        2.0 * (nrmvec * view_dirs).sum(-1, keepdims=True).clamp(min=0.0) * nrmvec - view_dirs
     )  # [1, H, W, 3]
 
     ambient = dr.texture(light.diffuse[None, ...], nrmvec.contiguous(), filter_mode='linear', boundary_mode='cube')
     specular_linear = ambient * diff_col
+
+    # spec0 = specular_linear.squeeze().permute(2,0,1)
 
     NoV = saturate_dot(nrmvec, view_dirs)  # [1, H, W, 1]
     fg_uv = torch.cat((NoV, roughness), dim=-1)  # [1, H, W, 2]
@@ -344,18 +345,34 @@ def gshader_deferred_shading(
     spec = dr.texture(light.specular[0][None, ...], 
                           reflvec.contiguous(), 
                           mip=list(m[None, ...] for m in light.specular[1:]), 
-                        #   mip_level_bias=miplevel[:,:,None].permute(1,2,0).contiguous(), #[1,1,numG]
                           mip_level_bias=miplevel[..., 0], 
                           filter_mode='linear-mipmap-linear', 
                           boundary_mode='cube') # [1, H, W, 3]
 
+    # mask = (nrmvec != 0).all(0, keepdim=True) #[1,h,w,3] 都不是0，才为true
+    # zero_tensor = torch.zeros_like(reflvec).cuda()
+    # reflvec_masked = torch.where(mask, reflvec, zero_tensor)
+    # spec_masked = dr.texture(light.specular[0][None, ...], 
+    #                          reflvec_masked.contiguous(), 
+    #                          mip=list(m[None, ...] for m in light.specular[1:]),
+    #                          mip_level_bias=miplevel[..., 0], 
+    #                          filter_mode='linear-mipmap-linear',
+    #                          boundary_mode='cube') 
+
     reflectance = spec_col * fg_lookup[...,0:1] + fg_lookup[...,1:2]
+    # spec1 = (spec * reflectance).squeeze().permute(2,0,1)
+    
+    
     specular_linear += spec * reflectance
 
     extras = {"specular_color": specular_linear.squeeze().permute(2,0,1)}
 
+    # extras['spec0'] = spec0
+    # extras['spec1'] = spec1
+
     diffuse_linear = torch.sigmoid(diffuse_raw - np.log(3.0))
     extras["diffuse_color"] = diffuse_linear.squeeze().permute(2,0,1)
+    
 
     rgb = specular_linear + diffuse_linear
 

pbr_deffered_render.py
@@ -84,31 +84,33 @@ def render_set(model_path, name, iteration, views, gaussians, cubemap,  pipeline
 
         gt = view.original_image[0:3, :, :]
 
-
         mask = (render_pkg["rend_normal"] != 0).all(0, keepdim=True) #[1,h,w]
-        image = torch.where(mask, render_pkg["render"], background[:,None,None])
         if "cr_map" in render_pkg.keys() and render_pkg["cr_map"] is not None:
             residual = torch.where(mask, render_pkg["cr_map"], background[:,None,None])
             torchvision.utils.save_image(residual, os.path.join(residual_path, '{0:05d}'.format(idx) + ".png"))
         
-        torchvision.utils.save_image(image, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
+        torchvision.utils.save_image(render_pkg["render"], os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
         torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
         
 
-        diffuse_rgb, specular_rgb, albedo, roughness, specular = render_pkg["diffuse_color"], render_pkg["specular_color"], render_pkg["kd_map"], render_pkg["kr_map"], render_pkg["ks_map"]
+
+        kd_map, ks_map, kr_map = render_pkg["kd_map"], render_pkg["ks_map"], render_pkg["kr_map"]
+        diffuse_rgb, specular_rgb = render_pkg["diffuse_color"], render_pkg["specular_color"]  
         
+        pbr_image = torch.cat([render_pkg["render"], diffuse_rgb, specular_rgb], dim=2)  # [3, H, 3W]
+        torchvision.utils.save_image(pbr_image, os.path.join(pbr_path, f"{idx:05d}_pbr.png"))
+
         diffuse_rgb = torch.where(mask, diffuse_rgb, background[:,None,None])
         specular_rgb = torch.where(mask, specular_rgb, background[:,None,None])
 
-        albedo = torch.where(mask, albedo, torch.zeros_like(albedo))
-        roughness = torch.where(mask, roughness, torch.zeros_like(roughness))
-        specular = torch.where(mask, specular, torch.zeros_like(specular))
+        albedo = torch.where(mask, kd_map, torch.zeros_like(kd_map))
+        roughness = torch.where(mask, kr_map, torch.zeros_like(kr_map))
+        specular = torch.where(mask, ks_map, torch.zeros_like(ks_map))
 
 
         brdf_map = torch.cat([albedo, roughness, specular,], dim=2,)
         torchvision.utils.save_image(brdf_map, os.path.join(brdf_path, f"{idx:05d}_brdf.png"))
-        pbr_image = torch.cat([image, diffuse_rgb, specular_rgb], dim=2)  # [3, H, 3W]
-        torchvision.utils.save_image(pbr_image, os.path.join(pbr_path, f"{idx:05d}_pbr.png"))
+
 
 
         # alpha = apply_depth_colormap(render_pkg["rend_alpha"][0][...,None], min=0., max=1.).permute(2,0,1)